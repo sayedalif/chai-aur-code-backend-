5d6dd69d62d587076d908b33899c3922c05d88dd
Alif <sayedalif18@gmail.com>
2024-08-31 06:36:31 +0600

    add getWatch history endpoint

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -359,6 +359,55 @@ const getUserChannelProfile = (asyncHandler(async (req, res) => {
     );
 }));
 
+const getWatchHistory = (asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user?._id)
+      }
+    },
+    {
+      $lookup: {
+        from: 'videos',
+        localField: 'watchHistory',
+        foreignField: '_id',
+        as: 'watchHistory',
+        pipeline: [
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'owner',
+              foreignField: '_id',
+              as: 'owner',
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                },
+                {
+                  $addFields: {
+                    owner: {
+                      $first: '$owner'
+                    }
+                  }
+                }
+              ],
+            }
+          }
+        ]
+      }
+    }
+  ]);
+
+  res.status(200)
+    .send(
+      new ApiResponse(200, user[0].watchHistory, 'watch history fetched successfully')
+    );
+}));
+
 export {
-  registerUser, loginUser, logoutUser, refreshTokenAccess, changeCurrentPassword, updateAccountDetails, getCurrentUser, updateUserAvatar, updateUserCoverImage, getUserChannelProfile
+  registerUser, loginUser, logoutUser, refreshTokenAccess, changeCurrentPassword, updateAccountDetails, getCurrentUser, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory
 };
